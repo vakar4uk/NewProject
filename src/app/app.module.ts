c02fd29dc0ae85c7e4cc5bd90d633d97f7593031
vakar4uk <31296706+vakar4uk@users.noreply.github.com>
2017-10-03 16:50:44 -0500

    df

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes =[
+  { path: '', component: LoginComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,    
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    BsDropdownModule.forRoot()
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    BsDropdownModule.forRoot(appRoutes),    
   ],
   providers: [],
   bootstrap: [AppComponent]
