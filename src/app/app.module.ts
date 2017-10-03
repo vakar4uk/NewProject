4916f8970b7ba71c75d4cf6f1c0292cd5eb0cbd1
vakar4uk <31296706+vakar4uk@users.noreply.github.com>
2017-10-03 16:51:01 -0500

    Revert "df"
    This reverts commit c02fd29dc0ae85c7e4cc5bd90d633d97f7593031.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { Routes, RouterModule } from '@angular/router';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-
-const appRoutes: Routes =[
-  { path: '', component: LoginComponent }
-];
 
 @NgModule({
   declarations: [
-    AppComponent,    
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule,
-    RouterModule.forRoot(appRoutes),
-    BsDropdownModule.forRoot(appRoutes),    
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
