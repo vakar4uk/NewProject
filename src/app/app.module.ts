f2553e78a0f88eecffcaf7fa01c910bc13ead9ef
savegadmytro@gmail.com <339613Dimond>
2017-10-03 21:55:34 -0500

    ver. 1.0  Login Page added
    Works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes =[
+  { path: '', component: LoginComponent },
+  { path: 'home', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
     BsDropdownModule.forRoot()
   ],
   providers: [],
