0d3b1906d6c2352be939e774dbaff830897779df
crkaiser <32202021+crkaiser@users.noreply.github.com>
2017-10-17 11:11:41 -0500

    typo fixes

--- nodejs/app.js
+++ nodejs/app.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/Persons', Persons);
-app.use('/UserTemps', UserT);
+app.use('/UserTemps', UserTemps);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
