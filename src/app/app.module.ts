e0ec6de9692839b1f625ae9584c195f7422ad6da
vakar4uk <31296706+vakar4uk@users.noreply.github.com>
2017-10-23 17:07:10 -0500

    asd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { AngularFontAwesomeModule } from 'angular-font-awesome/angular-font-awesome';
 import { ModalModule } from 'ngx-bootstrap/modal';
 
+
+
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
@@ -15,6 +18,11 @@ import { SchedulerComponent } from './scheduler/scheduler.component';
 import { SearchComponent } from './search/search.component';
 import { DoctorhomeComponent } from './doctorhome/doctorhome.component';
 import { EditpatientComponent } from './editpatient/editpatient.component';
+
+import {AccordionModule} from 'primeng/primeng';     //accordion and accordion tab
+import {MenuItem} from 'primeng/primeng';            //api
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ScheduleModule} from 'primeng/primeng';
 
 const appRoutes: Routes =[  
   { path: '', component: LoginComponent },
@@ -42,12 +50,12 @@ const appRoutes: Routes =[
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    
+    // BrowserAnimationsModule,
+    FormsModule,    
     AngularFontAwesomeModule,    
     RouterModule.forRoot(appRoutes),
     BsDropdownModule.forRoot(),
-    ModalModule.forRoot()    
+    ModalModule.forRoot()   
   ],
   providers: [],
   bootstrap: [AppComponent]
