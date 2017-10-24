534143dcb91febd6fb4b2d9cc4998010926c4ba2
aphang <adriennephang@gmail.com>
2017-10-24 11:35:38 -0500

    Read only functionality for fields DEAR DMYTRO PLS READ DESCRIPTION
    DMYTRO PLS CHANGE UPDOWN ARROWS PLS TTHNQQQQ

--- src/app/editlabresults/editlabresults.component.ts
+++ src/app/editlabresults/editlabresults.component.ts
@@ -11,5 +11,41 @@ export class EditlabresultsComponent implements OnInit {
 
   ngOnInit() {
   }
+  changeStatusSodium(){
+    (<HTMLInputElement>document.getElementById('sodium')).readOnly=false;
+    (<HTMLInputElement>document.getElementById('sodium')).focus();
+  }
+  changeStatusSodium2(){
+    (<HTMLInputElement>document.getElementById('sodium')).readOnly=true;
+  }
+  changeStatusPotassium(){
+    (<HTMLInputElement>document.getElementById('lastName')).readOnly=false;
+    (<HTMLInputElement>document.getElementById('lastName')).focus();
+  }
+  changeStatusPotassium2(){
+    (<HTMLInputElement>document.getElementById('lastName')).readOnly=true;
+  }
+  changeStatusCalcium(){
+    (<HTMLInputElement>document.getElementById('dob')).readOnly=false;
+    (<HTMLInputElement>document.getElementById('dob')).focus();    
+  }
+  changeStatusCalcium2(){
+    (<HTMLInputElement>document.getElementById('dob')).readOnly=true;
+  }
+  changeStatusGlucose(){
+    (<HTMLInputElement>document.getElementById('street')).readOnly=false;
+    (<HTMLInputElement>document.getElementById('street')).focus();    
+  }
+  changeStatusGlucose2(){
+    (<HTMLInputElement>document.getElementById('street')).readOnly=true;
+  }
+  changeStatusHemoglobin(){
+    (<HTMLInputElement>document.getElementById('unit')).readOnly=false;
+    (<HTMLInputElement>document.getElementById('unit')).focus();    
+  }
+  changeStatusHemoglobin2(){
+    (<HTMLInputElement>document.getElementById('unit')).readOnly=true;
+  }
+  
 
 }
