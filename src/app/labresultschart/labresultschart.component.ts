5734448693f780d7d45cf55ef9eb431262539153
aphang <adriennephang@gmail.com>
2017-10-26 16:13:06 -0500

    Color background change

--- src/app/labresultschart/labresultschart.component.ts
+++ src/app/labresultschart/labresultschart.component.ts
@@ -6,9 +6,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-labresultschart',
   templateUrl: './labresultschart.component.html',
-  styleUrls: ['./labresultschart.component.css']
+  styleUrls: ['./labresultschart.component.css'],
+  // template: `
+  // <base-chart
+  // class="chart"
+  // [datasets]="datasets"
+  // [colors]="colors"
+  // [labels]="labels"
+  // [options]="options"
+  // [chartType]="'bar'">
+  // </base-chart>
+  // `,
 })
 export class LabresultschartComponent {
+  // private colors = [
+  //   {
+  //     backgroundColor: [
+  //       'rgba(255, 99, 132, 0.2)',
+  //       'rgba(54, 162, 235, 0.2)',
+  //       'rgba(255, 206, 86, 0.2)',
+  //       'rgba(23,23,23,1)',
+  //       'rgba(102, 0, 204, 0.2)'
+  //     ]
+  //   }
+  //   ];
 
   public barChartOptions:any = {
     scaleShowVerticalLines: false,
@@ -20,7 +41,10 @@ export class LabresultschartComponent {
   
   public barChartData:any[] = [
     {data: [65, 59, 80, 81, 56, 55, 40], label: 'Sodium'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Potassium'}
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Potassium'},
+    {data: [80, 59, 12, 29, 89, 25, 14], label: 'Calcium'},
+    {data: [25, 44, 53, 73, 29, 82, 26], label: 'Glucose'},
+    {data: [10, 16, 38, 19, 35, 36, 15], label: 'Hemoglobin'},
   ];
   
   // events
