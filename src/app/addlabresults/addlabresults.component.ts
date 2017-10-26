e1df8b489900721443c9a2d1fe9470998d400563
charliep4147 <32108295+charliep4147@users.noreply.github.com>
2017-10-26 14:02:51 -0500

    Twice fix?

--- src/app/addlabresults/addlabresults.component.ts
+++ src/app/addlabresults/addlabresults.component.ts
@@ -8,7 +8,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
-import {TemplateRef } from '@angular/core';
+import { TemplateRef } from '@angular/core';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/modal-options.class';
 interface person {
@@ -29,14 +29,14 @@ interface personArray {
   [index: number]: person;
 }
 interface blood {
-  ResultsNo:any;
-  Sodium:any;
-  Potassium:any;
-  Calcium:any;
-  Glucose:any;
-  Hemoglobin:any;
-  Results_PID:any;
-  DateTaken:any;
+  ResultsNo: any;
+  Sodium: any;
+  Potassium: any;
+  Calcium: any;
+  Glucose: any;
+  Hemoglobin: any;
+  Results_PID: any;
+  DateTaken: any;
 
 }
 interface bloodArray {
@@ -53,32 +53,32 @@ export class AddlabresultsComponent implements OnInit {
   @ViewChild('f') addLabResults: NgForm;
   public isTableHidden: boolean = true;
   public isInfoHidden: boolean = true;
-  public pArray:personArray;
-  public date:any;
+  public pArray: personArray;
+  public date: any;
   personUrl = "http://localhost:3000/Persons";
-  bloodUrl= "http://localhost:3000/BloodTests";
+  bloodUrl = "http://localhost:3000/BloodTests";
   public modalRef: BsModalRef;
-  constructor(private modalService: BsModalService,private http: HttpClient, public _datatask: DataService, private _router: Router, private route: ActivatedRoute) { }
+  constructor(private modalService: BsModalService, private http: HttpClient, public _datatask: DataService, private _router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    console.log(this._datatask.ID);    
+    console.log(this._datatask.ID);
   }
   search(template: TemplateRef<any>) {
     //searching for person|patient
-    
+
     var search = (<HTMLInputElement>document.getElementById("search")).value;
     console.log(search);
     this.http.get<personArray>(this.personUrl + "/" + search).subscribe(data => {
       console.log("Looking for:" + search);
       console.log(data);
-      if(data[0] != undefined){
+      if (data[0] != undefined) {
         this.isTableHidden = false;
       }
-      else if(data[0] === undefined){
+      else if (data[0] === undefined) {
         this.modalRef = this.modalService.show(template);
       }
-      this.pArray=data;
-      console.log("Checking if data was Stored",this.pArray);
+      this.pArray = data;
+      console.log("Checking if data was Stored", this.pArray);
 
     },
       err => {
@@ -88,32 +88,54 @@ export class AddlabresultsComponent implements OnInit {
     );
 
   }
-  getID(index){
+  getID(index) {
     //get PID of person selected
     this._datatask.getID(this.pArray[index].PID);
     this.isInfoHidden = false;
   }
-  onSubmit(template: TemplateRef<any>) {
+  addRecord(template: TemplateRef<any>) {
     var date = (<HTMLInputElement>document.getElementById("testDate")).value;
-    var sodium=(<HTMLInputElement>document.getElementById("sodium")).value;
-    var potassium= (<HTMLInputElement>document.getElementById("potassium")).value;
-    var calcium=(<HTMLInputElement>document.getElementById("calcium")).value;
-    var glucose=(<HTMLInputElement>document.getElementById("glucose")).value;
-    var hemoglobin=(<HTMLInputElement>document.getElementById("hemoglobin")).value;
+    var sodium = (<HTMLInputElement>document.getElementById("sodium")).value;
+    var potassium = (<HTMLInputElement>document.getElementById("potassium")).value;
+    var calcium = (<HTMLInputElement>document.getElementById("calcium")).value;
+    var glucose = (<HTMLInputElement>document.getElementById("glucose")).value;
+    var hemoglobin = (<HTMLInputElement>document.getElementById("hemoglobin")).value;
     //if date exists get us template
-    this.http.get<bloodArray>(this.bloodUrl + "/" + date+"/"+this._datatask.ID).subscribe(data => {
+    this.http.get<bloodArray>(this.bloodUrl + "/" + date + "/" + this._datatask.ID).subscribe(data => {
       console.log("Looking for:" + date);
-      if(data[0] != undefined){
+      if (data[0] != undefined) {
         this.modalRef = this.modalService.show(template);
       }
-      
-    console.log(this.addLabResults);
+
+      console.log(this.addLabResults);
     },
-    err=>{
-      console.log("Adding Entry");
-      this._datatask.addBloodT(sodium,potassium,calcium,glucose,hemoglobin,this._datatask.ID,date);
-    }
-  );
+      err => {
+        console.log("Adding Entry");
+        this._datatask.addBloodT(sodium, potassium, calcium, glucose, hemoglobin, this._datatask.ID, date);
+      }
+    );
+  }
+  onSubmit() {
+    //   var date = (<HTMLInputElement>document.getElementById("testDate")).value;
+    //   var sodium=(<HTMLInputElement>document.getElementById("sodium")).value;
+    //   var potassium= (<HTMLInputElement>document.getElementById("potassium")).value;
+    //   var calcium=(<HTMLInputElement>document.getElementById("calcium")).value;
+    //   var glucose=(<HTMLInputElement>document.getElementById("glucose")).value;
+    //   var hemoglobin=(<HTMLInputElement>document.getElementById("hemoglobin")).value;
+    //   //if date exists get us template
+    //   this.http.get<bloodArray>(this.bloodUrl + "/" + date+"/"+this._datatask.ID).subscribe(data => {
+    //     console.log("Looking for:" + date);
+    //     if(data[0] != undefined){
+    //       this.modalRef = this.modalService.show(template);
+    //     }
+
+    //   console.log(this.addLabResults);
+    //   },
+    //   err=>{
+    //     console.log("Adding Entry");
+    //     this._datatask.addBloodT(sodium,potassium,calcium,glucose,hemoglobin,this._datatask.ID,date);
+    //   }
+    // );
   }
 
 }
