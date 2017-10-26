f18be4bee946ea41b975556f3731d2a548517988
charliep4147 <32108295+charliep4147@users.noreply.github.com>
2017-10-26 10:25:19 -0500

    checking global

--- src/app/addlabresults/addlabresults.component.ts
+++ src/app/addlabresults/addlabresults.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { DataService } from '../backend/data.service';
+import { Subscription } from 'rxjs/Rx';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Rx';
+import { Router, ActivatedRoute } from '@angular/router';
+import *as myID from '../global-id';
+import 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import {TemplateRef } from '@angular/core';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/modal-options.class';
 
 @Component({
   selector: 'app-addlabresults',
@@ -10,12 +22,14 @@ export class AddlabresultsComponent implements OnInit {
 
   @ViewChild('f') addLabResults: NgForm;
 
-  constructor() { }
+  constructor(private modalService: BsModalService,private http: HttpClient, public _datatask: DataService, private _router: Router, private route: ActivatedRoute) { }
 
-  ngOnInit() {    
+  ngOnInit() {
+    console.log(this._datatask.ID);    
   }
 
   onSubmit() {
+
     console.log(this.addLabResults);
   }
 
