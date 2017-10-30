7709be35986bb3c4d84581a684bb1167a3ceeacb
crkaiser <32202021+crkaiser@users.noreply.github.com>
2017-10-30 16:25:59 -0500

    db tweaks

--- nodejs/models/Appointment.js
+++ nodejs/models/Appointment.js
@@ -8,7 +8,7 @@ var Appointment={
 		return db.query("select * from Appointment where ApptDate=?",[id],callback);
 	},
 	addAppt:function(Appointment,callback){
-		return db.query("insert into Appointment values(?,?,?,?)",[Appointment.ApptNo,Appointment.Appt_DrID,Appointment.ApptDate,Appointment.Booked],callback);
+		return db.query("insert into Appointment values(?,?,?,?,?)",[Appointment.ApptNo,Appointment.Appt_DrID,Appointment.ApptDate,Appointment.Booked,Appointment.ApptTime],callback);
 	},
 	removeAppt:function(id,callback){
 		return db.query("delete from Appointment where ApptNo=?",[id],callback);
