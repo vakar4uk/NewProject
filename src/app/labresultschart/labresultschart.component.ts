98744de968129bdb388eda954589aeb69143ab84
vakar4uk <31296706+vakar4uk@users.noreply.github.com>
2017-11-01 10:04:53 -0500

    buttons

--- src/app/labresultschart/labresultschart.component.ts
+++ src/app/labresultschart/labresultschart.component.ts
@@ -29,6 +29,26 @@ export class LabresultschartComponent {
   public chartClicked(e:any):void {
     console.log(e);
   }
+
+  // PRINT
+  print(): void {
+    let printContents, popupWin;
+    printContents = document.getElementById('printArea').innerHTML;
+    popupWin = window.open('', '_blank', 'top=0,left=0,height=100%,width=auto');
+    popupWin.document.open();
+    popupWin.document.write(`
+      <html>
+        <head>
+          <title>Print tab</title>
+          <style>
+          //........Customized style.......
+          </style>
+        </head>
+    <body onload="window.print();window.close()">${printContents}</body>
+      </html>`
+    );
+    popupWin.document.close();
+}
   
   public chartHovered(e:any):void {
     console.log(e);
