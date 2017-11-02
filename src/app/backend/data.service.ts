c718d0ad7f8e54558e63a3b8e82d79ed1787ec8a
charliep4147 <32108295+charliep4147@users.noreply.github.com>
2017-11-02 10:55:40 -0500

    test

--- src/app/backend/data.service.ts
+++ src/app/backend/data.service.ts
@@ -44,10 +44,10 @@ interface blood {
   DateTaken: any;
 
 }
-interface personArray{
+interface personArray extends Array<person>{
   [index:number]:person;
 }
-interface loginArray {
+interface loginArray extends Array<login>{
   [index: number]: login;
 }
 interface blood {
@@ -61,7 +61,7 @@ interface blood {
   DateTaken: any;
 
 }
-interface bloodArray {
+interface bloodArray extends Array<blood>{
   [index: number]: blood;
 }
 interface Appointment {
@@ -72,7 +72,7 @@ interface Appointment {
   ApptTime: any;
   Appt_PID: any;
 }
-interface appointmentArray {
+interface appointmentArray extends Array<Appointment>{
   [index: number]: Appointment;
 }
 interface Doctor{
@@ -91,15 +91,15 @@ export class DataService {
   bloodUrl = "http://localhost:3000/BloodTests";
   appointmentUrl = "http://localhost:3000/Appointments"
   doctorUrl= "http://localhost:3000/Doctors";
-  pArray:personArray;
-  bArray:bloodArray;
-  lArray:loginArray;
+  pArray:personArray = [];
+  bArray:bloodArray = [];
+  lArray:loginArray = [];
   dArray: doctorArray = [];
   pIndex;
   bIndex;
   ID;
   IsDoctor:boolean;
-  aArray:appointmentArray;
+  aArray:appointmentArray = [];
   constructor(private http: HttpClient, private _router: Router, private route: ActivatedRoute) {
   }
 
